8de0ab9df7fbeba15e637b1f9b026dbf449e8e95
saif580 <saifulhasan580@gmail.com>
2024-02-13 19:49:02 +0530

    improve the responsiveness of the login /signup form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+    <div className="absolute px-8 py-2 z-10">
+    <div className="absolute inset-0 bg-black opacity-15"></div>
       <img
         className="w-40"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
--- src/components/Login.js
+++ src/components/Login.js
@@ -17,17 +17,17 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="background-pic"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-60"></div>
-        <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-60"></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <form className="absolute w-3/12 p-10 text-white rounded-sm bg-black bg-opacity-85 my-36 mx-auto right-0 left-0">
+      <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 text-white rounded-sm bg-black bg-opacity-65 w-full max-w-md">
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign up"}
         </h1>
         <input
-          type="text"
+          type="email"
           placeholder="Email or phone number"
           className="bg-black bg-opacity-25 border rounded-lg p-4 w-full my-4"
+          required
         />
         {isSignInForm ? (
           ""
@@ -36,6 +36,7 @@ const Login = () => {
             type="text"
             placeholder="Full name"
             className="bg-black bg-opacity-25 border rounded-lg p-4 w-full my-4"
+            required
           />
         )}
 
@@ -43,6 +44,7 @@ const Login = () => {
           type="password"
           placeholder="Password"
           className="bg-black bg-opacity-25 border rounded-lg p-4 w-full my-4"
+          required
         />
         <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
           {isSignInForm ? "Sign In" : "Sign up"}
